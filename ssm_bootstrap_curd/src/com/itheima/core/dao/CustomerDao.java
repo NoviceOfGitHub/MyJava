c9c9eea9f6071313eb3bdbf03aeca3187b0eb3ad
NoviceOfGitHub <925732287@qq.com>
2019-06-15 21:16:45 +0800

    搭建ssm+bootstrap，实现对数据库数据的curd

---
+++ ssm_bootstrap_curd/src/com/itheima/core/dao/CustomerDao.java
@@ -0,0 +1,17 @@
+package com.itheima.core.dao;
+
+import java.util.List;
+
+import com.itheima.core.po.Customer;
+
+/**
+ * Customer接口 
+ */
+public interface CustomerDao {
+	//客户列表
+	public List<Customer> selectCustomerList(Customer customer);
+	//客户数
+	public Integer selectCustomerListCount(Customer customer);
+	//创建客户
+	public int createCustomer(Customer customer);
+}
---
+++ ssm_bootstrap_curd/src/com/itheima/core/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.itheima.core.service;
+
+import com.itheima.common.utils.Page;
+import com.itheima.core.po.Customer;
+
+public interface CustomerService {
+	//查询客户列表
+	public Page<Customer> findCustomerList(Integer page, Integer rows, 
+			String custName, String custSource, String custIndustry, String custLevel);
+	
+	public int createCustomer(Customer customer);
+}
